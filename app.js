837c54566776e1775999097f1c0be2737635513b
kunal-TrustGrid <kunalr@trustgrid.com>
2026-01-27 16:01:26 +0530

    added cors for frontend integration

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const connectMongo = require('./services/mongodb');
 const blogRoutes = require('./routes/blog.js');
 const publicBlogRoutes = require('./routes/publicBlog.js');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
